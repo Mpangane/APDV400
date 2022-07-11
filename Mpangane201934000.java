897a3c8b11e033c0b3a5ee0c5c5f8108b4852459
Mpangane <109072948+Mpangane@users.noreply.github.com>
2022-07-11 13:46:19 +0200

    Add files via upload

---
+++ Mpangane201934000.java
@@ -0,0 +1,11 @@
+public class Mpangane201934000{
+	public static void main (String[] args){
+
+		int a= 7;
+		int b= 21;
+		int c= a + b;
+
+		System.out.println("the sum of a= 7 + b= 21 is equal to " + c );
+
+	}
+}
